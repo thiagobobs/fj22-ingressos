ecfdc0ec2fa258da2a9606a0a4cf3132cac336fc
Thiago Machado <thiagobobs@gmail.com>
2019-03-13 09:50:51 -0300

    Exercício 2.11

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,9 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
 
 @Controller
 public class SessaoController {
@@ -47,9 +51,16 @@ public class SessaoController {
 			return form(sessaoForm.getSalaId(), sessaoForm);
 		}
 		
-		sessaoDao.save(sessaoForm.toSessao(salaDao, filmeDao));
+		Sessao sessao = sessaoForm.toSessao(salaDao, filmeDao);
 		
-		return new ModelAndView("redirect:/admin/sala/" + sessaoForm.getSalaId() + "/sessoes");
+		List<Sessao> sessoesDaSala = sessaoDao.buscaSessoesDaSala(sessao.getSala());
+		
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
+		if (gerenciador.cabe(sessao)) {
+			sessaoDao.save(sessaoForm.toSessao(salaDao, filmeDao));
+			return new ModelAndView("redirect:/admin/sala/" + sessaoForm.getSalaId() + "/sessoes");
+		}
+		return form(sessaoForm.getId(), sessaoForm);
 	}
 
 }
